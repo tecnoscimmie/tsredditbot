1241cbe60f2b2cb9319da9055171f02a424fa821
Gianguido Sora <g.sora4@gmail.com>
2017-03-22 14:55:53 +0100

    deleted files used for inline query handling fuck inline query.

--- support/InlineQueryReply.go
+++
@@ -1,7 +0,0 @@
-package support
-
-// InlineQueryReply is a reply ready to be sent to telegram
-type InlineQueryReply struct {
-	InlineQueryID string                     `json:"inline_query_id"`
-	Results       []InlineQueryResultArticle `json:"results"`
-}
